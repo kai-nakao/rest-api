3992967ebc7d3d69b822b6c1ffa81da9c9e4e8bc
kai-nakao <nkkai0803@gmail.com>
2023-08-22 08:19:40 +0900

    feat: add task repository and usecase

---
+++ usecase/task_usecase.go
@@ -0,0 +1,41 @@
+package usecase
+
+import (
+	"go-rest-api/model"
+	"go-rest-api/repository"
+)
+
+type ITaskUsecase interface {
+	GetAllTasks(userId uint) ([]model.TaskResponse, error)
+	GetTaskById(userId uint, taskId uint) (model.TaskResponse, error)
+	CreateTask(task *model.Task) (model.TaskResponse, error)
+	UpdateTask(task *model.Task, userId uint, taskId uint) (model.TaskResponse, error)
+	DeleteTask(userId uint, taskId uint) error
+}
+
+type taskUsecase struct {
+	tr repository.ITaskRepository
+}
+
+func NewTaskUsecase(tr repository.ITaskRepository) ITaskUsecase {
+	return &taskUsecase{tr}
+}
+
+func (tu *taskUsecase) GetAllTasks(userId uint) ([]model.TaskResponse, error) {
+	tasks := []model.Task{}
+	if err := tu.tr.GetAllTasks(&tasks, userId); err != nil {
+		return nil, err
+	}
+	resTasks := []model.TaskResponse{}
+	for _, v := range tasks {
+		t := model.TaskResponse{
+			ID:        v.ID,
+			Title:     v.Title,
+			CreatedAt: v.CreatedAt,
+			UpdatedAt: v.UpdatedAt,
+		}
+		resTasks = append(resTasks, t)
+	}
+	return resTasks, nil
+}
+
