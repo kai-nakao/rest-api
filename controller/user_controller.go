f7b8b6d9c207bbc5f4a76ac14d8b2d2660d6a70f
kai-nakao <nkkai0803@gmail.com>
2023-08-27 07:47:10 +0900

    feat: add task & user validation

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -31,7 +31,7 @@ func (uc *userController) SignUp(c echo.Context) error {
 	if err := c.Bind(&user); err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-	userRes, err := uc.uu.SignUp(&user)
+	userRes, err := uc.uu.SignUp(user)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
@@ -43,7 +43,7 @@ func (uc *userController) Login(c echo.Context) error {
 	if err := c.Bind(&user); err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-	tokenString, err := uc.uu.Login(&user)
+	tokenString, err := uc.uu.Login(user)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"go-rest-api/model"
 	"go-rest-api/repository"
+	"go-rest-api/validator"
 	"os"
 	"time"
 
@@ -11,20 +12,24 @@ import (
 )
 
 type IUserUsecase interface {
-	SignUp(user *model.User) (model.UserResponse, error)
-	Login(user *model.User) (string, error)
+	SignUp(user model.User) (model.UserResponse, error)
+	Login(user model.User) (string, error)
 }
 
 type userUsecase struct {
 	ur repository.IUserRepository
+	uv validator.IUserValidator
 }
 
 // DI IRepository for usecase
-func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
-	return &userUsecase{ur}
+func NewUserUsecase(ur repository.IUserRepository, uv validator.IUserValidator) IUserUsecase {
+	return &userUsecase{ur, uv}
 }
 
-func (uu *userUsecase) SignUp(user *model.User) (model.UserResponse, error) {
+func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	if err := uu.uv.UserValidate(user); err != nil {
+		return model.UserResponse{}, err
+	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
 		return model.UserResponse{}, err
@@ -40,7 +45,10 @@ func (uu *userUsecase) SignUp(user *model.User) (model.UserResponse, error) {
 	return resUser, nil
 }
 
-func (uu *userUsecase) Login(user *model.User) (string, error) {
+func (uu *userUsecase) Login(user model.User) (string, error) {
+	if err := uu.uv.UserValidate(user); err != nil {
+		return "", err
+	}
 	storedUser := model.User{}
 	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
 		return "", err
